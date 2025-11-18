a98f215b8d96b9a3a23797603d06336eb64eb4cc
Pablo Flego <pablinff@gmail.com>
2025-11-18 16:34:11 +0100

    fix: format code for consistency and readability

--- apps/api-nestjs/src/date/date.controller.ts
+++ apps/api-nestjs/src/date/date.controller.ts
@@ -5,12 +5,12 @@ export class DateController {
   @Get()
   getCurrentDate() {
     const now = new Date();
-    return ({ current: now })
+    return { current: now };
   }
 
   @Get('past')
   getPastDate() {
-    const past = new Date("2024-01-01");
-    return ({ past })
+    const past = new Date('2024-01-01');
+    return { past };
   }
 }
